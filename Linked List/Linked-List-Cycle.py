fba52b7221c97d8ace30b0ed3f38f3d69cf7416b
Jon-LaFlamme <jonlaflamme7@gmail.com>
2021-02-03 22:36:50 -0500

    Linked List Cycle Solution

---
+++ Linked List/Linked-List-Cycle.py
@@ -0,0 +1,25 @@
+"""
+Difficulty: Easy
+
+Directons: Given a Linked List Head node, determine if a cycle exists in the list:
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if head is None:
+            return False
+        positions = set()
+        x = head
+        while x.next:
+            if x.next in positions:
+                return True
+            else:
+                positions.add(x.next)
+            x = x.next
+        return False
